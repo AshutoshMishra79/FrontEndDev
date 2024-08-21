b0505635ee0892cf0e5b4424e5c5761a12574a40
rkm20001712 <ritesh973mishra@gmail.com>
2024-08-21 05:55:08 +0530

    fix

---
+++ change-bg-color-unlimited/color_unlimited.js
@@ -0,0 +1,29 @@
+//generate a randonm number
+
+const randomColor = function () {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  
+  //console.log(randomColor());
+  let intervalId;
+  const startChangingColor = function () {
+    intervalId = setInterval(changeBgColor, 1000);
+  
+    function changeBgColor() {
+      document.body.style.backgroundColor = randomColor();
+      // console.log(randomColor());
+    }
+  };
+  const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+  };
+  
+  document.getElementById("start").addEventListener("click", startChangingColor);
+  document.getElementById("stop").addEventListener("click", stopChangingColor);
+  
